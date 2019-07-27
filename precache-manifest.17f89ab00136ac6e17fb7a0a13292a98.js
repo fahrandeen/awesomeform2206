b367de0d5944851e7a0f5f62389b1f41d31af111
Fahran Deen <fahrandeen17@gmail.com>
2019-07-27 14:27:16 +0530

    Updates

---
+++ precache-manifest.17f89ab00136ac6e17fb7a0a13292a98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2174bf547672c356697afb4dfde6ebe9",
+    "url": "/awesomeform2206/index.html"
+  },
+  {
+    "revision": "1359c7aabbcc99fb86ec",
+    "url": "/awesomeform2206/static/css/main.be2b127c.chunk.css"
+  },
+  {
+    "revision": "f6002ebedd4f178a4587",
+    "url": "/awesomeform2206/static/js/2.0c7ee168.chunk.js"
+  },
+  {
+    "revision": "1359c7aabbcc99fb86ec",
+    "url": "/awesomeform2206/static/js/main.0dff8d4d.chunk.js"
+  },
+  {
+    "revision": "578b280e77a7e1742ff4",
+    "url": "/awesomeform2206/static/js/runtime~main.c7f3ef64.js"
+  }
+]);
